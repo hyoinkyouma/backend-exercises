8007748362102d880cf3ec6d28fc5268793ae34c
Roman Cabalum <roman.cabalum@gmail.com>
2022-10-03 20:23:11 +0800

    change solution to match problem parameters

--- unique_in_order.rb
+++ unique_in_order.rb
@@ -1,16 +1,32 @@
+#def unique_in_order (iterable)
+# resArr = []
+
+#  if iterable.is_a? String
+#    iterable = iterable.split("")
+#  end
+
+#  iterable.each { |k, it|
+#    if not resArr.include? k
+#      resArr.push(k)
+#    end
+#    }
+#  return resArr
+#end
+
+#new solution
 def unique_in_order (iterable)
   resArr = []
 
-  if iterable.is_a? String
-    iterable = iterable.split("")
+  (0...iterable.length).each do |it|
+    if not iterable[it] == iterable[it + 1]
+      resArr.push(iterable[it])
+    end
   end
 
-  iterable.each { |k, it|
-    if not resArr.include? k
-      resArr.push(k)
-    end
-    }
   return resArr
+
 end
+
+puts unique_in_order('ABBCcAD')
 
 puts unique_in_order('ABBCcAD')
